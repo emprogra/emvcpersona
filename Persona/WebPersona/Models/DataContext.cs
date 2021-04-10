398aefc864929b1aa118958d39c6f4642c5ba63c
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-04-10 11:06:58 -0400

    Se agrego al menu Web Index

---
+++ Persona/WebPersona/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebPersona.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebPersona.Models.Person> People { get; set; }
+    }
+}
